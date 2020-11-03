486fad200133b32b1b9a9fccbf99606834b9157c
sandman <bmsandoval@gmail.com>
2020-11-02 20:48:41 -0700

    everything updated this time

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -74,13 +74,13 @@ for PLATFORM in $PLATFORMS; do
   GOARCH=${PLATFORM#*/}
   BIN_FILENAME="${OUTPUT}-${GOOS}-${GOARCH}"
 #  if [[ "${GOOS}" == "windows" ]]; then BIN_FILENAME="${BIN_FILENAME}.exe"; fi
-  CMD="GOOS=${GOOS} GOARCH=${GOARCH} go build -o builds/${BIN_FILENAME} ."
+  CMD="GOOS=${GOOS} GOARCH=${GOARCH} go build -mod=vendor -a -installsuffix cgo -o builds/${BIN_FILENAME} ."
   echo "${CMD}"
   eval $CMD || FAILURES="${FAILURES} ${PLATFORM}"
 done
 # ARM builds
 if [[ $PLATFORMS_ARM == *"linux"* ]]; then
-  CMD="GOOS=linux GOARCH=arm64 go build -o builds/${OUTPUT}-linux-arm64 ."
+  CMD="GOOS=linux GOARCH=arm64 go build -mod=vendor -a -installsuffix cgo -o builds/${OUTPUT}-linux-arm64 ."
   echo "${CMD}"
   eval $CMD || FAILURES="${FAILURES} ${PLATFORM}"
 fi
@@ -89,7 +89,7 @@ for GOOS in $PLATFORMS_ARM; do
   # build for each ARM version
   for GOARM in 7 6 5; do
     BIN_FILENAME="${OUTPUT}-${GOOS}-${GOARCH}${GOARM}"
-    CMD="GOARM=${GOARM} GOOS=${GOOS} GOARCH=${GOARCH} go build -o builds/${BIN_FILENAME} ."
+    CMD="GOARM=${GOARM} GOOS=${GOOS} GOARCH=${GOARCH} go build -mod=vendor -a -installsuffix cgo -o builds/${BIN_FILENAME} ."
     echo "${CMD}"
     eval "${CMD}" || FAILURES="${FAILURES} ${GOOS}/${GOARCH}${GOARM}"
   done
